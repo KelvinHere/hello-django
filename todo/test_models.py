a20758dfd9ef04992e28e026cb14cbf2f0c93591
Kelvin <kwardeu@gmail.com>
2020-11-20 10:47:30 +0000

    TESTING: Add models test

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Item
 
+class TestModels(TestCase):
+
+    def test_done_field_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)  # Check done defaults to false
 
